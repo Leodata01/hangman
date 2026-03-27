2840a60368569042ff8900130072f6efde3a0a27
leo <187560246+Leodata01@users.noreply.github.com>
2026-03-27 16:57:04 +0700

    add option to save a game using yaml format add option to load a saved game

--- hangman.rb
+++ hangman.rb
@@ -3,6 +3,7 @@
 # load dicationary file and select randomly a word between 5 and 12 chr
 
 module Hangman
+  require "yaml"
   ALPHABET = %w[a b c d e f g h i j k l m n o p q r s t u
                 v w x y z].freeze
 
@@ -19,18 +20,20 @@ module Hangman
       "    |––––––|\n    O      |\n   \\|/     |\n    |      |\n   / \\     |"
     ].freeze
     def initialize
-      @secrect_word = generate_secret_word
+      @secret_word = generate_secret_word
       @player = Player.new(self)
       @available_letters = ALPHABET
       @correct_letters = []
       @wrong_letters = []
-      @secrect_guess = Array.new(@secrect_word.length)
+      @secret_guess = Array.new(@secret_word.length)
       @round = 1
       @wrong_guess = false
+      @game_saved = 1
     end
     attr_reader :available_letters
 
     def play
+      option_load_game
       loop do
         play_round
         break if game_finished?
@@ -38,11 +41,53 @@ module Hangman
       winner_announcement
     end
 
+    def variables_to_yaml
+      [@secret_word,
+      @available_letters,
+      @correct_letters,
+      @wrong_letters,
+      @secret_guess,
+      @round,
+      @wrong_guess,
+      @game_saved].to_yaml
+    end
+
+    def save_game
+      File.write("save_#{@game_saved}.yaml", variables_to_yaml) 
+      @game_saved += 1
+    end 
+
+    def load_saved_game(number)
+      v = YAML.load_file("save_#{number}.yaml")
+      @secrect_word = v[0]
+      @available_letters = v[1]
+      @correct_letters = v[2]
+      @wrong_letters = v[3]
+      @secret_guess = v[4]
+      @round = v[5]
+      @wrong_guess = v[6]
+      @game_saved = v[7]
+    end
+
+    def load_game? 
+      print "Do you want to load your presvious game? y/n: "
+      yes_no_loop
+    end
+
+    def option_load_game
+      if load_game?
+        print "enter the number of your game saved: "
+        number = gets.chomp.to_i 
+        load_saved_game(number)
+      end 
+    end
+
+
     def winner_announcement
       if word_found?
-        puts "\nWell done !\nYou found the secret word: '#{@secrect_word.join}'"
+        puts "\nWell done !\nYou found the secret word: '#{@secret_word.join}'"
       else
-        puts "\nYou lost, the secret word was: '#{@secrect_word.join}'"
+        puts "\nYou lost, the secret word was: '#{@secret_word.join}'"
       end
     end
 
@@ -55,6 +100,7 @@ module Hangman
       display_drawing_and_secret_lines
       loop do
         guess_letters
+        option_to_save_game        
         break if wrong_guess?
       end
       @round += 1
@@ -69,6 +115,28 @@ module Hangman
       categorize_picked_letter(letter)
       update_secret_guess
       announce_result(letter)
+    end
+
+    def option_to_save_game
+      if save_game?
+        puts "You have a new save number: #{@game_saved}"
+        save_game
+      end
+    end
+
+    def yes_no_loop
+      answer = ""
+      loop do 
+        answer = gets.chomp.downcase
+        break if (answer=="y") || (answer=="n")
+        puts "Error, try 'y' or 'n'"
+      end
+      answer == "y"
+    end
+
+    def save_game?
+      print "Do you want to save your game? y/n : "
+      yes_no_loop
     end
 
     def categorize_picked_letter(letter)
@@ -94,7 +162,7 @@ module Hangman
     end
 
     def correct_letter?(letter)
-      @secrect_word.include? letter.downcase
+      @secret_word.include? letter.downcase
     end
 
     def pick_letter
@@ -103,8 +171,8 @@ module Hangman
     end
 
     def update_secret_guess
-      @secrect_word.each_with_index do |letter, i|
-        @secrect_guess[i] = letter if @correct_letters.include? letter
+      @secret_word.each_with_index do |letter, i|
+        @secret_guess[i] = letter if @correct_letters.include? letter
       end
     end
 
@@ -113,7 +181,7 @@ module Hangman
     end
 
     def word_found?
-      @secrect_guess == @secrect_word
+      @secret_guess == @secret_word
     end
 
     def max_round?
@@ -134,7 +202,7 @@ module Hangman
     def display_secret_lines
       print "\nSecret word => "
       secret_lines = ''
-      @secrect_guess.each do |letter|
+      @secret_guess.each do |letter|
         secret_lines += if letter.nil?
                           '_ '
                         else
@@ -178,4 +246,12 @@ module Hangman
   end
 end
 
-# Hangman::Game.new.play
+
+# File.open("saved_game")
+
+
+
+Hangman::Game.new.play
+
+
+
